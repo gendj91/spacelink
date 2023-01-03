aa645d1398ffc8c42c663feba777d7e129719504
gendj91 <92550696+gendj91@users.noreply.github.com>
2023-01-02 19:30:25 -0600

    initial loading of files
    adding everything for the project

---
+++ app.js
@@ -0,0 +1,25 @@
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        } else {
+            entry.target.classList.remove('show');
+        }
+    });
+});
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
+
+const transition = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add('inverted-hidden');
+        } else {
+            entry.target.classList.remove('inverted-hidden');
+        }
+    });
+});
+const observer2 = document.querySelectorAll('.inverted');
+observer2.forEach((el) => transition.observe(el));
